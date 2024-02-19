4d0057a2a47d545fea8c0d9e61139981f086ed45
beatrizmaciel <beatrizcbmaciel@gmail.com>
2024-02-19 14:19:46 +0000

    adding py file

---
+++ encrypt.py
@@ -0,0 +1,36 @@
+def replace(i, key, start, end):
+    n = end - start + 1
+    k = (i + key)%(end+1) + ((i + key)//(end+1))*start
+
+    if i + key < start:
+        k = k + n
+
+    return chr(k)
+
+def encrypt(msg, key):
+    # ord is a unicode character
+    cA, cZ, ca, cz = ord('A'), ord('Z'), ord('a'), ord('z')
+    encrypted = ""
+
+    for character in msg:
+        i = ord(character)
+        new_character = character
+
+        if cA <= i <= cZ:
+            new_character = replace(i, key, cA, cZ)
+        
+        elif i in range(ca, cz + 1):
+            new_character = replace(i, key, ca, cz)
+        
+        encrypted = encrypted + new_character
+    
+    return encrypted
+
+def decrypt(msg, key):
+    return encrypt(msg, -key)
+
+key = int(input("Insert a number: "))
+msg = input("Insert a text: ")
+
+encrypted = encrypt(msg, key)
+print(encrypted)
